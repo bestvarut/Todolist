f81d651b427de7c6513fb8b00fb561a4779c74db
bestvarut <51030057+bestvarut@users.noreply.github.com>
2021-06-08 15:17:44 +0700

    basic component

---
+++ client/src/components/page/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const About = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p className='my-1'>this is a full stack todolist App</p>
+    </div>
+  );
+};
+export default About;
